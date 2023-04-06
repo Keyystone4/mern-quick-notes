e8f201a23d4c027964eb771eaf02e6b710aa2fc5
Kristone Brown <Keyystone4@gmail.com>
2023-04-06 13:42:58 -0500

    associate functions with note form

--- src/pages/NewNotePage/NewNotePage.jsx
+++ src/pages/NewNotePage/NewNotePage.jsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 export default function NewNotePage() {
+  const [note, setNote] = useState('');
+
+  function handleChange(evt) {
+    setNote(evt.target.value)
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    alert(note)
+  }
+
   return (
-    <h1>New Note</h1>
+    <>
+      <h1>New Note</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="text">Text:
+          <input type="text" name="note" id="text" onChange={handleChange} />
+        </label>
+        <input type="submit" value="Add Note" />
+      </form>
+    </>
   );
 }
